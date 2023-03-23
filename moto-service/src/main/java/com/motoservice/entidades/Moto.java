d0128f53b6a24236e515476455852265a084eea3
CJIMENEZB <carlosjimenezb1986@gmail.com>
2023-03-23 15:57:06 -0500

    dsd

--- moto-service/src/main/java/com/motoservice/entidades/Moto.java
+++ moto-service/src/main/java/com/motoservice/entidades/Moto.java
@@ -1,14 +1,13 @@
 package com.motoservice.entidades;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
